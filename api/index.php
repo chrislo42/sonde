d86d7fa9517b01e92bd8fb7b92b07a4017f20e73
chrislo42 <chris42.lopez@gmail.com>
2017-04-03 17:03:44 +0200

    ajout nouvelles routes sur index. création de l'appli cliente

--- api/index.php
+++ api/index.php
@@ -3,6 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+require 'config.php';
 
 spl_autoload_register(function ($classname) {
     require ("../classes/" . $classname . ".php");
@@ -11,10 +12,10 @@ spl_autoload_register(function ($classname) {
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
 
-$config['db']['host']   = "localhost";
-$config['db']['user']   = "slimapp";
-$config['db']['pass']   = "access";
-$config['db']['dbname'] = "nodemcu";
+$config['db']['host']   = DB_HOST;
+$config['db']['user']   = DB_USER;
+$config['db']['pass']   = DB_PASSWD;
+$config['db']['dbname'] = DB_NAME;
 
 $app = new \Slim\App(["settings" => $config]);
 
@@ -49,29 +50,117 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     return $response;
 });
 
-$app->get('/meas/last', function (Request $request, Response $response) {
+$app->get('/meas/last_html', function (Request $request, Response $response) {
 	$req = $this->db->query('SELECT * FROM mesures ORDER BY date  DESC LIMIT 1');
     $donnees = $req->fetch();
 	$temp = $donnees['temp'];
 	$humid = $donnees['humid'];
     $req->closeCursor(); // Termine le traitement de la requête
-	$this->logger->addInfo("get last measure");
+	$this->logger->addInfo("get html last measure");
     $response->getBody()->write("Last measurement => température : $temp ; humidité : $humid");
 
     return $response;
 });
 
-$app->get('/meas/last_json', function (Request $request, Response $response) {
-	$req = $this->db->query('SELECT * FROM mesures ORDER BY date  DESC LIMIT 1');
+$app->get('/meas/day/last', function (Request $request, Response $response) {
+    $req = $this->db->query('SELECT * FROM mesures ORDER BY date  DESC LIMIT 1');
     $donnees = $req->fetch();
-	$temp = $donnees['temp'];
-	$humid = $donnees['humid'];
+    $reponse = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
     $req->closeCursor(); // Termine le traitement de la requête
-	$this->logger->addInfo("get json last measure");
-    $response = array('temp'=> $temp, 'humid'=> $humid);
+    $this->logger->addInfo("get last measure");
 
-    return json_encode($response);
+    return json_encode($reponse);
 });
+
+$app->get('/meas/day/current', function (Request $request, Response $response) {
+	$req = $this->db->prepare('SELECT * FROM mesures WHERE date  > ?');
+	$date = date("Y-m-d")." 00:00:00";
+    $req->execute(array($date));
+    $listes = array();
+    while ($donnees = $req->fetch()) {
+        $listes[] = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
+    }
+    $req->closeCursor(); // Termine le traitement de la requête
+	$this->logger->addInfo("get current day measures");
+
+    return json_encode($listes);
+});
+
+$app->get('/meas/day/{date}', function (Request $request, Response $response) {
+    $date = $request->getAttribute('date');
+    $date1 = $date." 00:00:00";
+    $date2 = $date." 23:59:59";
+    $req = $this->db->prepare('SELECT * FROM mesures WHERE date BETWEEN ? AND ?');
+    $req->execute(array($date1, $date2));
+    $listes = array();
+    while ($donnees = $req->fetch()) {
+        $listes[] = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
+    }
+    $req->closeCursor(); // Termine le traitement de la requête
+    $this->logger->addInfo("get given day measures");
+
+    return json_encode($listes);
+});
+
+$app->get('/meas/days/{date1}/{date2}', function (Request $request, Response $response) {
+    $date1 = $request->getAttribute('date1')." 00:00:00";
+    $date2 = $request->getAttribute('date2')." 23:59:59";
+    $req = $this->db->prepare('SELECT * FROM mesures WHERE date BETWEEN ? AND ?');
+    $req->execute(array($date1, $date2));
+    $listes = array();
+    while ($donnees = $req->fetch()) {
+        $listes[] = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
+    }
+    $req->closeCursor(); // Termine le traitement de la requête
+    $this->logger->addInfo("get interval days measures");
+
+    return json_encode($listes);
+});
+
+$app->get('/meas/month/current', function (Request $request, Response $response) {
+    $req = $this->db->prepare('SELECT * FROM mesures WHERE date  > ?');
+    $date = date("Y-m")."-00 00:00:00";
+    $req->execute(array($date));
+    $listes = array();
+    while ($donnees = $req->fetch()) {
+        $listes[] = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
+    }
+    $req->closeCursor(); // Termine le traitement de la requête
+    $this->logger->addInfo("get current month measures");
+
+    return json_encode($listes);
+});
+
+$app->get('/meas/month/{month1}', function (Request $request, Response $response) {
+    $req = $this->db->prepare('SELECT * FROM mesures WHERE date BETWEEN ? AND ?');
+    $date1 = $request->getAttribute('month1')."-00 00:00:00";
+    $date2 = $request->getAttribute('month1')."-31 23:59:59";
+    $req->execute(array($date1, $date2));
+    $listes = array();
+    while ($donnees = $req->fetch()) {
+        $listes[] = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
+    }
+    $req->closeCursor(); // Termine le traitement de la requête
+    $this->logger->addInfo("get given month measures");
+
+    return json_encode($listes);
+});
+
+$app->get('/meas/months/{month1}/{month2}', function (Request $request, Response $response) {
+    $date1 = $request->getAttribute('month1')."-00 00:00:00";
+    $date2 = $request->getAttribute('month2')."-31 23:59:59";
+    $req = $this->db->prepare('SELECT * FROM mesures WHERE date BETWEEN ? AND ?');
+    $req->execute(array($date1, $date2));
+    $listes = array();
+    while ($donnees = $req->fetch()) {
+        $listes[] = array('date'=> $donnees['date'], 'temp'=> $donnees['temp'], 'humid'=> $donnees['humid']);
+    }
+    $req->closeCursor(); // Termine le traitement de la requête
+    $this->logger->addInfo("get interval months measures");
+
+    return json_encode($listes);
+});
+
 $app->post('/meas/new', function (Request $request, Response $response) {
 	$data = $request->getParsedBody();
 	$temp = $data['value1'];
@@ -79,8 +168,8 @@ $app->post('/meas/new', function (Request $request, Response $response) {
 	$req = $this->db->prepare('INSERT INTO mesures (temp, humid) VALUES (?,?)');
 	$req->execute(array($temp,$humid));
 	$req->closeCursor(); // Termine le traitement de la requête
-	$this->logger->addInfo("post new measure, temp : $temp humid : $humid");
-    $response->getBody()->write("New measurement !!!=> température : $temp; humidité : $humid");
+	$this->logger->addInfo("post new measures, temp : $temp humid : $humid");
+    $response->getBody()->write("Get new measures !!! => temp : $temp; humid : $humid");
 
     return $response;
 });
---
+++ api/sample_config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 30/03/17
+ * Time: 09:38
+ *
+ * Exemple de fichier de configuration pour l'api sonde en REST
+ *
+ */
+
+/**
+ * @const DB_HOST	URL de la base de données
+ * @const DB_USER	Username de la base de données
+ * @const DB_PASSWD	Mot de passe de username
+ * @const DB_NAME   Nom de la bas de données
+ */
+define('DB_HOST',"xxxxxxx");
+define('DB_USER',"xxxxxxx");
+define('DB_PASSWD',"xxxxxxx");
+define('DB_NAME',"xxxxxxx");
+
+?>
